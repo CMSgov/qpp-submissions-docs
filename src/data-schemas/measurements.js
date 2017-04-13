1523a6f7eda94cc617469cb92726c22991a10550
Mari Miyachi <mari@navahq.com>
2017-04-12 21:06:19 -0700

    Docs for single performance rate measurements
    Closes #10
    
    Reviewer: @kencheeto

--- src/data-schemas/measurements.js
+++ src/data-schemas/measurements.js
@@ -18,7 +18,15 @@ var PROPORTION_FIELDS = [
   {name: 'denominator', value: 'integer', description: 'The total number of patients. Must be greater than or equal to zero.', notes: 'writable'},
 ];
 
-var PERFORMANCE_RATE_FIELDS = [
+var SINGLE_PERFORMANCE_RATE_FIELDS = [
+  {name: 'isEndToEndReported', value: 'boolean', description: 'True if the measure was reported  via certified EHR technology without any manual interference.', notes: 'writable'},
+  {name: 'performanceMet', value: 'integer', description: 'The number of patients for which the measure criteria are satisfied. Must be greater than or equal to zero and less than or equal to the <b>populationTotal</b>', notes: 'writable'},
+  {name: 'performanceNotMet', value: 'integer', description: 'The number of patients for which the measure criteria are not satisfied.', notes: 'writable, optional'},
+  {name: 'performanceExclusion', value: 'integer', description: 'The number of patients who are excluded from the measure.', notes: 'writable, optional'},
+  {name: 'populationTotal', value: 'integer', description: 'The total number of patients. Must be greater than or equal to zero.', notes: 'writable'}
+];
+
+var MULTI_PERFORMANCE_RATE_FIELDS = [
   {name: 'isEndToEndReported', value: 'boolean', description: 'True if the measure was reported  via certified EHR technology without any manual interference.', notes: 'writable'},
   {name: 'strata', value: 'list', description: 'The strata associated with the performance rate measurement.', notes: 'writable'}
 ];
@@ -29,7 +37,7 @@ var STRATA_FIELDS = [
   {name: 'performanceNotMet', value: 'integer', description: 'The number of patients for which the measure criteria are not satisfied.', notes: 'writable, optional'},
   {name: 'performanceExclusion', value: 'integer', description: 'The number of patients who are excluded from the measure.', notes: 'writable, optional'},
   {name: 'populationTotal', value: 'integer', description: 'The total number of patients. Must be greater than or equal to zero.', notes: 'writable'},
-  {name: 'stratum', value: 'string', description: 'The strata associated with the performance rate measurement.', notes: 'writable, optional if only one stratum in the performance rate measurement'}
+  {name: 'stratum', value: 'string', description: 'The strata associated with the performance rate measurement.', notes: 'writable'}
 ];
 
 class Measurements extends React.PureComponent {
@@ -39,7 +47,7 @@ class Measurements extends React.PureComponent {
     return (
        <div>
           <h1 className="ds-h1">Measurements</h1>
-          <p className="ds-text--lead">The Measurements resource represents performance data for a specific measure within a MeasurementSet. There are three types of Measurements: Boolean, Proportion and Performance Rate. Each MeasurementSet can have multiple Measurements. No two Measurements in a given MeasurementSet can have the same measureId.</p>
+          <p className="ds-text--lead">The Measurements resource represents performance data for a specific measure within a MeasurementSet. There are three types of Measurements: Boolean, Proportion, Single-Performance Rate and Multi-Performance Rate. Each MeasurementSet can have multiple Measurements. No two Measurements in a given MeasurementSet can have the same measureId.</p>
           <p className="ds-text--lead">For a list of methods for this resource, view the <a href="https://qpp-submissions-sandbox.navapbc.com/#/Measurements">OpenAPI documentation</a>.</p>
           <h2 className="ds-h2">Resource Representation</h2>
           <div className='markup markup--html'>
@@ -49,7 +57,8 @@ class Measurements extends React.PureComponent {
   "measurementSetId": string,
   "measureId": string,
   "value": [`}
-    <a href="#boolean">Boolean</a> | <a href="#proportion">Proportion</a> | <a href="#performance-rate">Performance Rate</a>
+    <a href="#boolean">Boolean</a> | <a href="#proportion">Proportion</a> | <a href="#single-performance-rate">Single-Performance Rate</a> | {`
+  `} <a href="#multi-performance-rate">Multi-Performance Rate</a>
   {`]
 }`}
             </pre>
@@ -86,8 +95,28 @@ class Measurements extends React.PureComponent {
             </pre>
           </div>
           <DataModelTable fields={PROPORTION_FIELDS} />
-          <h1 className="ds-h1" id="performance-rate">Performance Rate Measurements</h1>
-          <p className="ds-text--lead">Performance Rate Measurements are applicable to Quality measures. There are two types of Performance Rate Measurements: single and multi. Single-Performance Rate Measurements contain only one stratum. Multi-Performance Rate Measurements contain multiple strata and the stratum field is required for each.</p>
+          <h1 className="ds-h1" id="single-performance-rate">Single-Performance Rate Measurements</h1>
+          <p className="ds-text--lead">Single-Performance Rate Measurements are applicable to Quality measures.</p>
+          <h2 className="ds-h2">Resource Representation</h2>
+          <div className='markup markup--html'>
+            <pre className='ds-u-border--1 ds-u-padding--1'>
+              {`{
+  "id": string,
+  "measurementSetId": string,
+  "measureId": string,
+  "value": {
+    "isEndToEndReported": boolean,
+    "performanceMet": integer,
+    "performanceNotMet": integer,
+    "performanceExclusion": integer,
+    "populationTotal": integer
+  }
+}`}
+            </pre>
+          </div>
+          <DataModelTable fields={SINGLE_PERFORMANCE_RATE_FIELDS} />
+          <h1 className="ds-h1" id="multi-performance-rate">Multi-Performance Rate Measurements</h1>
+          <p className="ds-text--lead">Multi-Performance Rate Measurements are applicable to Quality measures. Multi-Performance Rate Measurements contain multiple strata and the stratum field is required for each.</p>
           <h2 className="ds-h2">Resource Representation</h2>
           <div className='markup markup--html'>
             <pre className='ds-u-border--1 ds-u-padding--1'>
@@ -104,9 +133,9 @@ class Measurements extends React.PureComponent {
 }`}
             </pre>
           </div>
-          <DataModelTable fields={PERFORMANCE_RATE_FIELDS} />
-          <h1 className="ds-h1" id="stratum">Performance Rate Stratum</h1>
-          <p className="ds-text--lead">A Performance Rate Stratum represents the performance data for a specified subset of the population, as described by the stratum field. In the case where the stratum field is null, the performance data is applicable to the entire population.</p>
+          <DataModelTable fields={MULTI_PERFORMANCE_RATE_FIELDS} />
+          <h1 className="ds-h1" id="stratum">Multi-Performance Rate Stratum</h1>
+          <p className="ds-text--lead">A Multi-Performance Rate Stratum represents the performance data for a specified subset of the population, as described by the stratum field.</p>
           <h2 className="ds-h2">Resource Representation</h2>
           <div className='markup markup--html'>
             <pre className='ds-u-border--1 ds-u-padding--1'>
