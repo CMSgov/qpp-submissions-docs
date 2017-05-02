7bb62ef35bdaaf4dd9dd27c435688884526ca525
Sung Choi <m.sung.choi@gmail.com>
2017-05-02 11:30:34 -0400

    Updates wording in scoring and benchmark docs.

--- src/components/api-reference/scoring.jsx
+++ src/components/api-reference/scoring.jsx
@@ -14,7 +14,7 @@ export default class ScoringEngine extends PureComponent {
         <h1 className="ds-h1">Scoring</h1>
         <p className="ds-text--lead">The scoring engine package (@cmsgov/qpp-scoring-engines) used by the qpp-submissions-api to score submissions can be found <a href="https://github.com/CMSgov/qpp-scoring-engines">here</a> on github.</p>
         <h2 className="ds-h2">Method Signature</h2>
-        <p className="ds-text--lead">Improvement activities (IA), advancing care information (ACI), and quality measures are scored differently and as such the scoring engine package provides three distinct scoring engines, all with the same method signature.</p>
+        <p className="ds-text--lead">Improvement activities (IA), advancing care information (ACI), and quality measures are scored differently. The scoring engine package provides three distinct scoring engines, all with the same method signature.</p>
         <div className='markup markup--html'>
           <pre className='ds-u-border--1 ds-u-padding--1'>
             {`calculateImprovementActivitiesScore(submission, provider, benchmarks)
