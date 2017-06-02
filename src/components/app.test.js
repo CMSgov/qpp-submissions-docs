5c7dea2851060c009a92cbe1a38c4ca6dabca9a0
Kenshiro Nakagawa <kencheeto@gmail.com>
2017-06-02 14:46:28 -0700

    move test to right place, add travis.yml

--- src/components/app.test.js
+++ src/components/app.test.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../src/components/app';
+import App from './app';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <App url={document.URL}/>,
+    div
+  );
 });
