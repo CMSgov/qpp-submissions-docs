81df5b735e6130618798afc7090d742e3a28f1ee
Sung Choi <sungchoi@users.noreply.github.com>
2017-06-05 12:57:44 -0400

    QPPA-668 Adds performanceRate and reportingRate to schema Corrects information about performanceRate for single performance rate measurements and adds details to the reportingRate and performanceRate descriptions.

--- src/components/api-reference/schemas/measurements.jsx
+++ src/components/api-reference/schemas/measurements.jsx
@@ -23,8 +23,8 @@ const SINGLE_PERFORMANCE_RATE_FIELDS = [
   {name: 'performanceNotMet', value: 'integer', description: 'The number of patients for which the measure criteria are not satisfied.', notes: 'writable, optional'},
   {name: 'performanceExclusion', value: 'integer', description: 'The number of patients who are excluded from the measure.', notes: 'writable, optional'},
   {name: 'populationTotal', value: 'integer', description: 'The total number of eligible patients as described by the measure. Must be greater than or equal to zero.', notes: 'writable'}
-  {name: 'reportingRate', value: 'float', description: 'The quotient that results from dividing the sum of performanceMet, performanceExclusion, and performanceNotMet by the populationTotal.', notes: ''},
-  {name: 'performanceRate', value: 'float', description: 'The performance rate is calculated based on the overallAlgorithm of the measure. Currently, a measure\'s overallAlgorithm can be simpleAverage, weightedAverage, or sumNumerators.',  notes: ''}
+  {name: 'reportingRate', value: 'float', description: 'The reporting rate, ranging from zero to one-hundred and representing a percentage, is equal to ((performanceMet + performanceExclusion + and performanceNotMet) / populationTotal) * 100: .', notes: ''},
+  {name: 'performanceRate', value: 'float', description: 'The performance rate, ranging from zero to one-hundred and representing a percentage, is equal to (performanceMet / (performanceMet + performanceNotMet)) * 100.',  notes: ''}
 ];
 
 const MULTI_PERFORMANCE_RATE_FIELDS = [
@@ -39,8 +39,8 @@ const STRATA_FIELDS = [
   {name: 'performanceExclusion', value: 'integer', description: 'The number of patients who are excluded from the measure.', notes: 'writable, optional'},
   {name: 'populationTotal', value: 'integer', description: 'The total number of eligible patients as described by the measure. Must be greater than or equal to zero.', notes: 'writable'},
   {name: 'stratum', value: 'string', description: 'The strata associated with the performance rate measurement.', notes: 'writable'},
-  {name: 'reportingRate', value: 'float', description: 'The quotient that results from dividing the sum of performanceMet, performanceExclusion, and performanceNotMet by the populationTotal.', notes: ''},
-  {name: 'performanceRate', value: 'float', description: 'The performance rate is calculated based on the overallAlgorithm of the measure. Currently, a measure\'s overallAlgorithm can be simpleAverage, weightedAverage, or sumNumerators.',  notes: ''}
+  {name: 'reportingRate', value: 'float', description: 'The reporting rate, ranging from zero to one-hundred and representing a percentage, is equal to ((performanceMet + performanceExclusion + and performanceNotMet) / populationTotal) * 100: .', notes: ''},
+  {name: 'performanceRate', value: 'float', description: 'The performance rate, ranging from zero to one-hundred and representing a percentage, is calculated based on the overallAlgorithm of the measure. Currently, a measure\'s overallAlgorithm can be a <b>simpleAverage</b>, <b>weightedAverage</b>, or <b>sumNumerators</b>.',  notes: ''}
 ];
 
 class Measurements extends React.PureComponent {
