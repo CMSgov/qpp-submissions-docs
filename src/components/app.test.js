3ac168123e7df64e5adb679fb2e8f84ef4566123
Kenshiro Nakagawa <kencheeto@gmail.com>
2017-07-27 14:38:19 -0400

    class -> className in jsx, add test for previewbeta route

--- src/components/app.test.js
+++ src/components/app.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {shallow, mount} from 'enzyme';
 import App from './app';
 import BasicTutorial from './tutorials/basic-tutorial';
+import DeveloperPreview from './developer-preview';
 
 it('renders without crashing', () => {
   shallow(<App url={document.URL}/>);
@@ -15,4 +16,11 @@ it('renders landing page by default', () => {
 it('routes to tutorial when the path ends with /tutorial', () => {
   const app = mount(<App url={document.URL + 'tutorial'}/>);
   expect(app.containsMatchingElement(<BasicTutorial />)).toEqual(true);
+});
+
+it('routes to developer preview when the path ends with /privatebeta', () => {
+  // we've sent out documentation with /privatebeta already, so we should
+  // continue to honor this route.
+  const app = mount(<App url={document.URL + 'privatebeta'}/>);
+  expect(app.containsMatchingElement(<DeveloperPreview />)).toEqual(true);
 });
