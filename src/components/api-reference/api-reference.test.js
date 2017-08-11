1b0c80a76cf5576ab3f99a4c9750d7ed5abbe254
Aimee Barciauskas <aimee@navahq.com>
2017-08-11 14:29:26 -0400

    Fix tests

--- src/components/api-reference/api-reference.test.js
+++ src/components/api-reference/api-reference.test.js
@@ -3,6 +3,7 @@ import {shallow, mount} from 'enzyme';
 import ApiReference from './api-reference';
 import Submission from './schemas/submission';
 import Benchmarks from './schemas/benchmarks';
+import Scoring from './scoring';
 
 it('renders without crashing', () => {
   shallow(<ApiReference />);
@@ -13,12 +14,14 @@ it('renders submission schema by default', () => {
   expect(component.containsMatchingElement(<Submission />)).toEqual(true);
 });
 
-it('routes to benchmarks when the hash is #historical-benchmarks', () => {
-  const component = mount(<ApiReference hash='#historical-benchmarks' />);
+it('routes to benchmarks when the activeComponent is benchmarks', () => {
+  const component = mount(<ApiReference />);
+  component.setState({activeComponent: 'benchmarks'});
   expect(component.containsMatchingElement(<Benchmarks />)).toEqual(true);
 });
 
-it('routes to benchmarks when the hash is #benchmark-calculations', () => {
-  const component = mount(<ApiReference hash='#benchmark-calculations' />);
-  expect(component.containsMatchingElement(<Benchmarks />)).toEqual(true);
+it('routes to benchmarks when the activeComponent is scoring', () => {
+  const component = mount(<ApiReference />);
+  component.setState({activeComponent: 'scoring'});
+  expect(component.containsMatchingElement(<Scoring />)).toEqual(true);
 });
