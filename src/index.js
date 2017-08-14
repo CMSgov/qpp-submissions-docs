0f3971d80dde18c23d22a0da77bb0a34ccfc16f9
Aimee Barciauskas <aimee@navahq.com>
2017-08-14 17:31:45 -0400

    Add comment about forceRefresh

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './components/app';
 import './styles/index.css';
 
 ReactDOM.render(
+  // According to the docs, forceRefresh is required for browsers which don't
+  // support pushState. However, even when this is set to false and 'pushState'
+  // in window.history is 'true', clicked links did not impact the content.
   <BrowserRouter forceRefresh={true} basename="/qpp-submissions-docs/">
     <App />
   </BrowserRouter>,
