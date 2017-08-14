c0ad41e80777585d879f38858a00db4d10966271
Aimee Barciauskas <aimee@navahq.com>
2017-08-14 18:22:48 -0400

    Add simple left nav

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../../node_modules/uswds/dist/js/uswds.js';
 import '../../node_modules/@cmsgov/design-system-core/dist/index.css';
-import { Route } from 'react-router-dom'
+import { Route, NavLink } from 'react-router-dom'
 
 import '../styles/app.css';
 import '../styles/temp-grid.css';
@@ -44,13 +44,26 @@ const guidesPathsMap = {
 
 const allPaths = Object.assign({}, topicsPathsMap, guidesPathsMap)
 
-const getComponent = ({ match }) => {
-  var activeComponent = allPaths[match.params.componentKey].component;
-  return <div>{activeComponent}</div>
+const getComponent = ({ match }) => (
+  <div>{allPaths[match.params.componentKey].component}</div>
+)
+
+const createLinksListItems = function(pathsMap) {
+  var linkListItems = [];
+  Object.keys(pathsMap).forEach((pathKeyName) => {
+    linkListItems.push(
+      // <NavLink> is special version of the <Link> that will add styling attributes to the rendered element when it matches the current URL.
+      <li key={pathKeyName}><NavLink to={"/" + pathKeyName} activeClassName="active">{pathsMap[pathKeyName].linkText}</NavLink></li>
+    )
+  });
+  return linkListItems;
 }
 
 class App extends React.PureComponent {
   render() {
+    const topicsNavItems = createLinksListItems(topicsPathsMap);
+    const guidesNavItems = createLinksListItems(guidesPathsMap);
+
     return (
       <div>
         <a className="usa-skipnav" href="#main-content">Skip to main content</a>
@@ -59,8 +72,24 @@ class App extends React.PureComponent {
           <a href="/qpp-submissions-docs" title="Home" aria-label="Home">QPP Submissions API <br/> Developer Documentation</a>
         </div>
         <div className="container">
-          <Route exact path="/" component={Introduction} />
-          <Route path="/:componentKey" render={getComponent} />
+          <div className="temp-grid-container">
+            <div className="ds-u-float--left ds-u-padding-right--6 ds-u-padding-top--2">
+              <ul className="ds-c-vertical-nav__subnav">
+                <li className="ds-c-vertical-nav__item"><b>TOPICS</b></li>
+                <ul className="ds-c-vertical-nav__subnav">
+                  {topicsNavItems}
+                </ul>
+                <li className="ds-c-vertical-nav__item"><b>GUIDES</b></li>
+                <ul className="ds-c-vertical-nav__subnav">
+                  {guidesNavItems}
+                </ul>
+              </ul>
+            </div>
+            <div className="ds-u-float--left ds-u-padding--1 page">
+              <Route exact path="/" component={Introduction} />
+              <Route path="/:componentKey" component={getComponent} />
+            </div>
+          </div>
         </div>
         <script src="/assets/js/vendor/uswds.min.js"></script>
       </div>
