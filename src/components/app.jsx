75cf77daaefa0685158c502c466b5c737fcaba5e
Aimee Barciauskas <aimee@navahq.com>
2017-08-15 12:14:36 -0400

    Add NavSection react component and refactor appropriately

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -11,43 +11,42 @@ import Header from './header';
 import Routes from './routes';
 import Introduction from './introduction';
 
-const allPaths = Object.assign({}, Routes.topics, Routes.guides, Routes.references)
-
-const getComponent = ({ match, location }) => {
-  return <div>{allPaths[match.params.componentKey].component}</div>
-}
+const mergedRoutes = Object.values(Routes).reduce(function(result, routesGroup) {
+  return Object.assign(result, routesGroup);
+}, {});
 
 const createLinksListItems = function(pathsMap) {
-  var linkListItems = [];
-  Object.keys(pathsMap).forEach((pathKeyName) => {
-    linkListItems.push(
+  return Object.keys(pathsMap).reduce((result, pathKeyName) => {
+    return result.concat(
       // <NavLink> is special version of the <Link> that will add styling attributes to the rendered element when it matches the current URL.
       <li key={pathKeyName}>
         <NavLink to={"/" + pathKeyName} activeClassName="active">{pathsMap[pathKeyName].linkText}</NavLink>
       </li>
     )
-    // if there are sections, add ul starting and ending tags and a link for each
-    // FIXME: may want this conditional to be a bit more specific, e.g. test for a non-empty array
-    const subSections = pathsMap[pathKeyName].sections;
-    if (subSections !== undefined) {
-      linkListItems.push(
-        <ul key={pathKeyName + '-subList'}>
-          {Object.keys(subSections).map((subKey) => {
-            return <li key={subKey}><a href={"/qpp-submissions-docs/" + pathKeyName + '#' + subKey}>{subSections[subKey].linkText}</a></li> 
-          })}
-        </ul>
-      )
-    }
-  });
-  return linkListItems;
+  }, []);
+}
+
+class NavSection extends React.Component {
+  render() {
+    return <li className="ds-c-vertical-nav__item">
+      <b>{this.props.name.toUpperCase()}</b>
+      <ul className="ds-c-vertical-nav__subnav">
+        {createLinksListItems(Routes[this.props.name])}
+      </ul>
+    </li>;
+  }
+}
+
+const LeftNav = Object.keys(Routes).reduce(function(result, sectionName) {
+  return result.concat(<NavSection key={sectionName} name={sectionName}/>)
+}, []);
+
+const getComponent = ({ match, location }) => {
+  return <div>{mergedRoutes[match.params.componentKey].component}</div>
 }
 
 class App extends React.PureComponent {
   render() {
-    const topicsNavItems = createLinksListItems(Routes.topics);
-    const guidesNavItems = createLinksListItems(Routes.guides);
-    const referenceNavItems = createLinksListItems(Routes.references);
-
     return (
       <div>
         <a className="usa-skipnav" href="#main-content">Skip to main content</a>
@@ -59,18 +58,7 @@ class App extends React.PureComponent {
           <div className="temp-grid-container">
             <div className="ds-u-float--left ds-u-padding-right--6 ds-u-padding-top--2">
               <ul className="ds-c-vertical-nav__subnav">
-                <li className="ds-c-vertical-nav__item"><b>TOPICS</b></li>
-                <ul className="ds-c-vertical-nav__subnav">
-                  {topicsNavItems}
-                </ul>
-                <li className="ds-c-vertical-nav__item"><b>GUIDES</b></li>
-                <ul className="ds-c-vertical-nav__subnav">
-                  {guidesNavItems}
-                </ul>
-                <li className="ds-c-vertical-nav__item"><b>REFERENCE</b></li>
-                <ul className="ds-c-vertical-nav__subnav">
-                  {referenceNavItems}
-                </ul>
+                {LeftNav}
               </ul>
             </div>
             <div className="ds-u-float--left ds-u-padding--1 page">
