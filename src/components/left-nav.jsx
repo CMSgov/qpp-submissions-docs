050ef5aa491609a7880d9ba14957ae25ad41469c
Aimee Barciauskas <aimee@navahq.com>
2017-08-15 12:21:05 -0400

    Add left nav file

---
+++ src/components/left-nav.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom'
+import Routes from './routes';
+
+const createLinksListItems = function(pathsMap) {
+  return Object.keys(pathsMap).reduce((result, pathKeyName) => {
+    return result.concat(
+      // <NavLink> is special version of the <Link> that will add styling attributes to the rendered element when it matches the current URL.
+      <li key={pathKeyName}>
+        <NavLink to={"/" + pathKeyName} activeClassName="active">{pathsMap[pathKeyName].linkText}</NavLink>
+      </li>
+    )
+  }, []);
+}
+
+class NavSection extends React.Component {
+  render() {
+    return <li className="ds-c-vertical-nav__item">
+      <b>{this.props.name.toUpperCase()}</b>
+      <ul className="ds-c-vertical-nav__subnav">
+        {createLinksListItems(Routes[this.props.name])}
+      </ul>
+    </li>;
+  }
+}
+
+const LeftNav = Object.keys(Routes).reduce(function(result, sectionName) {
+  return result.concat(<NavSection key={sectionName} name={sectionName}/>)
+}, []);
+
+export default LeftNav;
