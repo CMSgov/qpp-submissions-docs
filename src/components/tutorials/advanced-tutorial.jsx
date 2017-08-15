a48d4cbbc893470e87b29fd3b769e3d8b4922b40
Sam Keller <sam@navahq.com>
2017-08-15 19:30:44 -0400

    QPPA-921: Add QPP header and footer to Developer Documentation
    Closes #51
    
    Adding the basic QPP header and footer to the developer documentation,
    with slight differences (which means we couldn't use the Header and
    Footer that come with the qpp-style repo) -- the header does't have the
    MIPS/APMs/About dropdowns and the footer has links, which are relative
    links in the qpp-style repo.
    
    Reviewer: @kencheeto

--- src/components/tutorials/advanced-tutorial.jsx
+++ src/components/tutorials/advanced-tutorial.jsx
@@ -50,8 +50,8 @@ class AdvancedTutorial extends React.PureComponent {
           <p className="ds-c-alert--text">Looks like the screen is too narrow to show you the API payloads alongside the tutorial. As you see references to code on the right, you'll be able to find it all the way at the bottom. Clicking the buttons to progress through the tutorial will update the code!</p>
         </div>
       </div>
-      <div className="temp-grid-container">
-        <div className="temp-grid-half">
+      <div className="container">
+        <div className="temp-grid">
           <h1 className="ds-h1">Advanced API Tutorial</h1>
           <p>In the <a href="/qpp-submissions-docs/tutorial">first tutorial</a> we covered how to create a submission, add a measurement set with IA category performance data, and retrieve the score in three different API requests. This time we're going to build on the previous tutorial and look at creating a submission with embedded ACI performance data in one request, go through ACI scoring and how measures compose a score, and see how to update a measure with new info (all while running into a problem along the way). All of these examples serve to illustrate how the Submissions API can make it easier to react to and fix issues that arise.</p>
           <p>
@@ -98,15 +98,14 @@ class AdvancedTutorial extends React.PureComponent {
                 <tr><td className="nested-twice">Measure <code>ACI_HIE_2</code></td>
                     <td>20 out of 100</td></tr>
               </tbody>
-            }
-            button={
-              <button
-                className="ds-c-button ds-c-button--primary"
-                data-hash="#submitting-with-performance-data"
-                onClick={this.showResponseOfStep}>
-                Create Submission
-              </button>
             }/>
+          <div className="temp-grid">
+            <TechnicalDetailsPane
+              tutorial="advanced"
+              hash="#submitting-with-performance-data"
+              tabIndex={this.state.tabIndex}
+              handleSelect={this.handleSelect}/>
+          </div>
           <p>Something unexpected: a <code>422 Unprocessable Entity</code> response code. This indicates that the syntax of the request was correct, but the semantics were problematic. The response body includes more specific information: <code>DuplicateEntryError</code>. We've tried to create a duplicate submission - earlier we noted that each taxpayer/provider ID combination can have one submission per year. The <code>POST</code> API request we just sent uses the same identifiers as we did in our first tutorial, but CMS already has a submission on record for this individual.</p>
           <p>There are a variety of reasons why this collision might happen: it's plausible that we (or someone else) has tried to <code>POST</code> this individual's performance data before, or someone made a typo and used our TIN by accident. Either way, we asked the API to <em>create</em> a record where one already exists. Since the API (and CMS) can't assume what the correct course of action is to take for this problematic API request, the messaging in the response is handy for immediately showing us something went wrong, and what specifically.</p>
           <p>If we wanted to <em>update</em> the existing submission we could use a <code>PUT</code> (full record update) or <code>PATCH</code> (partial record update) request, but since we're trying to show how we can create a new submission with measurement data embedded, let's use a different TIN and try again.</p>
@@ -141,15 +140,14 @@ class AdvancedTutorial extends React.PureComponent {
                 <tr><td className="nested-twice">Measure <code>ACI_HIE_2</code></td>
                     <td>20 out of 100</td></tr>
               </tbody>
-            }
-            button={
-              <button
-                className="ds-c-button ds-c-button--primary"
-                data-hash="#submitting-with-performance-data-pt2"
-                onClick={this.showResponseOfStep}>
-                Create Submission
-              </button>
             }/>
+          <div className="temp-grid">
+            <TechnicalDetailsPane
+              tutorial="advanced"
+              hash="#submitting-with-performance-data-pt2"
+              tabIndex={this.state.tabIndex}
+              handleSelect={this.handleSelect}/>
+          </div>
           <p>A <code>201 Created</code> - great. We'll look at what ACI scores look like next:</p>
           <button
             className="ds-c-button ds-c-button--primary"
@@ -171,15 +169,14 @@ class AdvancedTutorial extends React.PureComponent {
          <p>The scoring is more complicated for ACI measures, so we'll spend more time going through that.</p>
           <InlineApiExample
             verb="GET"
-            url="/submissions/:id/score"
-            button={
-              <button
-                className="ds-c-button ds-c-button--primary"
-                data-hash="#aci-scoring"
-                onClick={this.showResponseOfStep}>
-                Get Submission Score
-              </button>
-            }/>
+            url="/submissions/:id/score"/>
+          <div className="temp-grid">
+            <TechnicalDetailsPane
+              tutorial="advanced"
+              hash="#aci-scoring"
+              tabIndex={this.state.tabIndex}
+              handleSelect={this.handleSelect}/>
+          </div>
           <p>The ACI component of our score shows many parts this time. There's a lot of numbers here, but let's decode how we arrive at the total:</p>
           <p>The <code>aci_base</code> starts at 50 because we've attested to the two required measures (<code>ACI_INFBLO_1</code>, <code>ACI_ONCDIR_1</code>).</p>
           <p>Three optional measures listed (<code>ACI_PEA_1</code>, <code>ACI_HIE_1</code>, <code>ACI_HIE_2</code>) give a possible 10 points each, multiplied by the proportion attested to, leading to partial contributions of 5, 2, and 1 points respectively.</p>
@@ -217,15 +214,14 @@ class AdvancedTutorial extends React.PureComponent {
                 <tr><td>Proportion</td>
                     <td>50 out of 100</td></tr>
               </tbody>
-            }
-            button={
-              <button
-                className="ds-c-button ds-c-button--primary"
-                data-hash="#updating-a-measure"
-                onClick={this.showResponseOfStep}>
-                Update Measurement
-              </button>
             }/>
+          <div className="temp-grid">
+            <TechnicalDetailsPane
+              tutorial="advanced"
+              hash="#updating-a-measure"
+              tabIndex={this.state.tabIndex}
+              handleSelect={this.handleSelect}/>
+          </div>
           <p>A <code>200 OK</code> means we've updated the measurement in question. We can now fetch the latest score:</p>
           <button
             className="ds-c-button ds-c-button--primary"
@@ -243,15 +239,14 @@ class AdvancedTutorial extends React.PureComponent {
           </h2>
           <InlineApiExample
             verb="GET"
-            url="/submissions/:id/score"
-            button={
-              <button
-                className="ds-c-button ds-c-button--primary"
-                data-hash="#comparing-scoring-changes"
-                onClick={this.showResponseOfStep}>
-                Get Submission Score
-              </button>
-            }/>
+            url="/submissions/:id/score"/>
+          <div className="temp-grid">
+            <TechnicalDetailsPane
+              tutorial="advanced"
+              hash="#comparing-scoring-engines"
+              tabIndex={this.state.tabIndex}
+              handleSelect={this.handleSelect}/>
+          </div>
           <p>A few things have changed - the final score increased to 15.5. We know this change is due to our PATCH by looking at the score component contributed by <code>ACI_HIE_1</code> - it increased from 1 to 5, since the proportion increased from 10 to 50 out of 100 with ten possible points from this measure. The ACI base score went up from 58 to 62, and with the ACI component being 25% of the score our final score increased by 1.</p>
           <p>It's important to note that we've been working in one measurement set for this tutorial. One of the reasons that performance data for measures is organized into measurement sets is that multiple submission methods can add their own measurement sets into one submission - measure scores that might overlap the ones we provide, or differ in their attested values. In the case of multiple measurement sets, the Submissions API will calculate scores for each measurement set and pick the highest to present as the final score - that's why the API identifies that the scoring for the ACI component is based on a specific measurement set ID.</p>
           <p>We've done a lot of work in 5 API requests! In this advanced tutorial, we created a submission with ACI performance attestations, scored it, and updated a piece of one measurement to see how the final score reacts. That means we've successfully used each API endpoint (submission, measurement set, measurement) and understood what the responses tell us, adjusting our approach as needed. Again, all of this work is done on our terms, at our pace - no months-long round trip required.</p>
@@ -260,13 +255,6 @@ class AdvancedTutorial extends React.PureComponent {
           <p>While we've used each API endpoint, we're far from having used every kind of API action. These tutorials have used <code>POST</code>, <code>PATCH</code>, and <code>GET</code> - there are also <code>PUT</code> and <code>DELETE</code> for each of the three resources we worked with.</p>
           <p>The tutorials have shown how requests can be strung together to complete complex workflows in minutes rather than months. </p>
           <p>Now, try using the <a href="https://qpp-submissions-sandbox.navapbc.com/">Interactive Docs</a> to experiment with some of your data.</p>
-        </div>
-        <div className="temp-grid-half">
-          <TechnicalDetailsPane
-            tutorial="advanced"
-            hash={this.state.hash}
-            tabIndex={this.state.tabIndex}
-            handleSelect={this.handleSelect}/>
         </div>
       </div>
       </div>
