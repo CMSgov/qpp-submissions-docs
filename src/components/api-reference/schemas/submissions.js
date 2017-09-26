46fd230f2610742c6804e677c26452accc161744
ivanang <ivana@navahq.com>
2017-09-25 23:22:11 -0400

    Clarify when npi is writable and get rid of details about NPI and TIN having to start with 0

--- src/components/api-reference/schemas/submissions.js
+++ src/components/api-reference/schemas/submissions.js
@@ -11,8 +11,8 @@ const FIELDS = [
   {name: 'updatedAt', value: 'datetime', description: 'The modification time of the submission in <a href="https://www.ietf.org/rfc/rfc3339.txt">RFC 3339</a> format.'},
   {name: 'programName', value: 'string', description: 'The quality payment program under which the submission belongs. Acceptable values are <b>"mips"</b>.', notes: 'writable'},
   {name: 'entityType', value: 'string', description: 'The type of provider associated with the submission. Acceptable values are <b>"individual"</b> and <b>"group"</b>.', notes: 'writable'},
-  {name: 'taxypayerIdentificationNumber', value: 'string', description: 'The 9-digit identifier of the provider associated with the submission. As the API does not yet collect personally identifying information, all TINs must begin with "000".', notes: 'writable'},
-  {name: 'nationalProviderIdentifier', value: 'string', description: 'The 10-digit identifier of the provider associated with the submission. As the API does not yet collect personally identifying information, all NPIs must begin with "0"', notes: 'writable, optional if entityType is "group" or entityId is provided'},
+  {name: 'taxypayerIdentificationNumber', value: 'string', description: 'The 9-digit identifier of the provider associated with the submission.', notes: 'writable'},
+  {name: 'nationalProviderIdentifier', value: 'string', description: 'The 10-digit identifier of the provider associated with the submission.', notes: 'writable only if entityType is "individual"'},
   {name: 'entityId', value: 'string', description: ' The identifier for Alternative Payment Models (APM)', notes: 'writable, optional'},
   {name: 'performanceYear', value: 'integer', description: 'The year in which performance data for the submission was collected.', notes: 'writable'},
   {name: 'measurementSets', value: 'array(measurementSet)', description: 'Measurement sets associated with the submission.', notes: 'writable, optional'}
