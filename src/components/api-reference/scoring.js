17a23fb3366360769827d888a9a439e2bac4a311
Michael Conroy <sietekk@gmail.com>
2017-11-08 15:16:52 -0500

    Complete except for XML snippets

--- src/components/api-reference/scoring.js
+++ src/components/api-reference/scoring.js
@@ -7,65 +7,69 @@ import '../../styles/common/example-code-tabs.css';
 import DataModelTable from './common/data-model-table';
 
 const SCORE_RESOURCE = {
+  id: 'score-resource',
   title: 'Score Resource',
   description: 'The Score resource represents the top level scoring data for a submission object. Each Score will have multiple Score Parts and may have multiple Score Warnings or Score Errors. The Score Detail field lists the current version of the Scoring Engine used to score the corresponding submission.',
-  example: `"score": {
-    "name": string,
-    "title": string,
-    "detail": string,
-    "value": number
-    "parts": array(ScorePart),
-    "metadata": object(ScoreMetadata),
-    "warnings": array(string),
-    "errors": array(string)
-  }`,
+  example: {
+    json: `{
+      "name": string,
+      "title": string,
+      "detail": string,
+      "value": number
+      "parts": array(ScorePart),
+      "metadata": object(ScoreMetadata),
+      "warnings": array(string),
+      "errors": array(string)
+    }`,
+    xml: `Coming soon!`
+  },
   fields: [
     {
       name: 'name',
       value: 'string',
-      description: 'name of score object',
+      description: 'Name of score object',
       notes: 'static'
     },
     {
       name: 'title',
       value: 'string',
-      description: 'title of score object',
+      description: 'Title of score object',
       notes: 'static'
     },
     {
       name: 'detail',
       value: 'string',
-      description: 'semantic version of scoring engine',
+      description: 'Semantic version of scoring engine',
       notes: 'Semantic version of the Scoring Engine used to compute the score Object'
     },
     {
       name: 'value',
       value: 'number',
-      description: 'total score of submission',
+      description: 'Total score of submission',
       notes: 'Final, total score for the submission'
     },
     {
       name: 'parts',
       value: 'array',
-      description: 'array ofScoreParts',
-      notes: 'Reweighted category score parts, which build the final, total score'
+      description: 'Array ofScoreParts',
+      notes: 'Performance category score parts, that contribute to total score'
     },
     {
       name: 'metadata',
       value: 'object',
-      description: 'top level scoring metadata',
+      description: 'Top level scoring metadata',
       notes: ''
     },
     {
       name: 'warnings',
       value: 'array',
-      description: 'array of Warnings',
+      description: 'Array of Warnings',
       notes: 'Scoring issues that do not halt the scoring process'
     },
     {
       name: 'errors',
       value: 'array',
-      description: 'array of Errors',
+      description: 'Array of Errors',
       notes: 'Scoring issues that halt the scoring process'
     }
   ],
@@ -125,39 +129,43 @@ const SCORE_RESOURCE = {
 };
 
 const SCORE_PART_RESOURCE = {
+  id: 'score-part-resource',
   title: 'Score Part Resource',
   description: 'The Score Part resource represents the Categorical scoring data for a submission object. Categorical scoring refers to one of the three QPP Performance Categories, which are IA, ACI or Quality.',
-  example: `[
-    "name": string,
-    "title": string,
-    "detail": string,
-    "value": number,
-    "original": object(CategoryScore),
-    "metadata": object(ScorePartMetadata)
-  ]`,
+  example: {
+    json: `[
+      "name": string,
+      "title": string,
+      "detail": string,
+      "value": number,
+      "original": object(CategoryScore),
+      "metadata": object(ScorePartMetadata)
+    ]`,
+    xml: `Coming soon!`
+  },
   fields: [
     {
       name: 'name',
       value: 'string',
-      description: 'category identifier',
+      description: 'Performance category identifier',
       notes: ''
     },
     {
       name: 'title',
       value: 'string',
-      description: 'title of score part object',
+      description: 'Title of score part object',
       notes: 'e.g., "ACI component of final score"'
     },
     {
       name: 'detail',
       value: 'string',
-      description: 'details category score weight',
+      description: 'Performance category score weight',
       notes: 'e.g., "Scoring based on weight of 15%"'
     },
     {
       name: 'value',
       value: 'number',
-      description: 'weighted category score',
+      description: 'Weighted category score',
       notes: ''
     },
     {
@@ -192,37 +200,41 @@ const SCORE_PART_RESOURCE = {
 };
 
 const CATEGORY_SCORE_RESOURCE = {
+  id: 'category-score-resource',
   title: 'Category Score Resource',
   description: 'The Category Score resource represents for each of the three individual category scoring engines, the scoring output for a category’s measurement sets contained in a submission.',
-  example: `"original": {
-    "name": string,
-    "value": number,
-    "detail": string,
-    "parts": array(MeasurementSetScorePart)
-  }`,
+  example: {
+    json: `{
+      "name": string,
+      "value": number,
+      "detail": string,
+      "parts": array(MeasurementSetScorePart)
+    }`,
+    xml: `Coming soon!`
+  },
   fields: [
     {
       name: 'name',
       value: 'string',
-      description: 'Category identifier',
+      description: 'Performance category identifier',
       notes: ''
     },
     {
       name: 'value',
       value: 'number',
-      description: 'Category score',
+      description: 'Performance category score',
       notes: ''
     },
     {
       name: 'detail',
       value: 'string',
-      description: 'Details the highest scoring measurement set by submission method',
+      description: 'Highest scoring measurementSet by submission method',
       notes: 'Should be moved to a metadata field with an identifier by measurement set ID'
     },
     {
       name: 'parts',
       value: 'array',
-      description: 'Scored measurement sets matching the category identifier',
+      description: 'Scored measurementSets matching the category identifier',
       notes: 'Sorted descending by score'
     },
     {
@@ -241,27 +253,31 @@ const CATEGORY_SCORE_RESOURCE = {
 };
 
 const MEASUREMENT_SET_SCORE_PART_RESOURCE = {
+  id: 'measurement-set-score-part-resource',
   title: 'Measurement Set Score Part Resource',
   description: 'The Scored Measurement Set represents the scoring output for each category measurement set in a submission.',
-  example: `[
-    "name": string,
-    "value": number,
-    "title": string,
-    "detail": string,
-    "parts": array(MeasurementScorePart | MeasurementScore),
-    "metadata": object(MeasurementSetScorePartMetadata)
-  ]`,
+  example: {
+    json: `[
+      "name": string,
+      "value": number,
+      "title": string,
+      "detail": string,
+      "parts": array(MeasurementScorePart | MeasurementScore),
+      "metadata": object(MeasurementSetScorePartMetadata)
+    ]`,
+    xml: `Coming soon!`
+  },
   fields: [
     {
       name: 'name',
       value: 'string',
-      description: 'Category identifier',
+      description: 'Performance category identifier',
       notes: ''
     },
     {
       name: 'value',
       value: 'number',
-      description: 'Category score if highest scoring measurement set',
+      description: 'Category score if highest scoring measurementSet',
       notes: ''
     },
     {
@@ -327,13 +343,13 @@ const MEASUREMENT_SET_SCORE_PART_RESOURCE = {
         {
           name: 'preAttestationCheck',
           value: 'string',
-          description: 'Messaging detailing completeness of pre-attestation checks',
+          description: 'Completeness of pre-attestation checks',
           notes: ''
         },
         {
           name: 'baseMeasureCheck',
           value: 'string',
-          description: 'Messaging detailing completeness of base measure checks',
+          description: 'Completeness of base measure checks',
           notes: ''
         }
       ]
@@ -343,25 +359,25 @@ const MEASUREMENT_SET_SCORE_PART_RESOURCE = {
         {
           name: 'measuresPicked',
           value: 'array',
-          description: 'Array of measure IDs, which were picked to count towards the quality base score',
+          description: 'Array of measure IDs, picked to count towards the quality base score',
           notes: 'These measures contribute to the base score, before bonuses are applied'
         },
         {
           name: 'totalMeasurementPoints',
           value: 'number',
-          description: 'The sum of individual measurement base scores whether picked or skipped',
+          description: 'Sum of individual measurement base scores whether picked or skipped',
           notes: ''
         },
         {
           name: 'totalBonusPoints',
           value: 'number',
-          description: 'The sum of individual measurement bonus scores whether the measurement was picked or skipped',
+          description: 'Sum of individual measurement bonus scores whether the measurement was picked or skipped',
           notes: ''
         },
         {
           name: 'denominator',
           value: 'number',
-          description: 'The denominator value used when calculating the measurement set score',
+          description: 'Denominator value used when calculating the measurement set score',
           notes: ''
         },
         {
@@ -373,7 +389,7 @@ const MEASUREMENT_SET_SCORE_PART_RESOURCE = {
         {
           name: 'reweightedScore',
           value: 'number',
-          description: 'A score as if this measurement set was chosen as its overall category score and sent to final scoring and reweighted',
+          description: 'Score as if this measurement set was chosen as its overall category score and sent to final scoring and reweighted',
           notes: ''
         },
         {
@@ -426,16 +442,20 @@ const MEASUREMENT_SET_SCORE_PART_RESOURCE = {
 };
 
 const MEASUREMENT_SCORE_PART_RESOURCE = {
+  id: 'measurement-score-part-resource',
   title: 'Measurement Score Part Resource',
   description: 'The Measurement Score Part resource represents the structured organization of Measurement Scores organized into score parts. ACI scoring has individual score parts, which scored measurements are grouped into unlike IA and Quality scores.',
-  example: `[
-    "name": string,
-    "value": number,
-    "detail": string,
-    "parts": array(MeasurementScore),
-    "metadata": object(MeasurementScorePartMetadata),
-    "warnings": array
-  ]`,
+  example: {
+    json: `[
+      "name": string,
+      "value": number,
+      "detail": string,
+      "parts": array(MeasurementScore),
+      "metadata": object(MeasurementScorePartMetadata),
+      "warnings": array
+    ]`,
+    xml: `Coming soon!`
+  },
   fields: [
     {
       name: 'name',
@@ -493,15 +513,19 @@ const MEASUREMENT_SCORE_PART_RESOURCE = {
 };
 
 const MEASUREMENT_SCORE_RESOURCE = {
+  id: 'measurement-score-resource',
   title: 'Measurement Score Resource',
   description: 'The Measurement Score resource represents the scoring result for each measurement within a submission’s measurement sets.',
-  example: `[
-    "name": string,
-    "title": string,
-    "value": number,
-    "detail": string,
-    "metadata": object
-  ]`,
+  example: {
+    json: `[
+      "name": string,
+      "title": string,
+      "value": number,
+      "detail": string,
+      "metadata": object
+    ]`,
+    xml: `Coming soon!`
+  },
   fields: [
     {
       name: 'name',
@@ -759,35 +783,41 @@ const MEASUREMENT_SCORE_RESOURCE = {
   }
 };
 
-const CodeBlock = ({code}) => {
-  const reformattedCode = code
-    .split('\n')
-    .map(l => l.trim())
-    .map((line, idx, arr) => {
-      if (idx > 0 && idx < arr.length - 1) {
-        return `  ${line}`;
-      }
+const reformattedCode = (code) => code
+  .split('\n')
+  .map(l => l.trim())
+  .map((line, idx, arr) => {
+    if (idx > 0 && idx < arr.length - 1) {
+      return `  ${line}`;
+    }
 
-      return line;
-    })
-    .join('\n');
+    return line;
+  })
+  .join('\n');
+
+const CodeBlock = ({json, xml}) => {
+  const reformattedJSON = reformattedCode(json);
+  const reformattedXML = reformattedCode(xml);
 
   return (
     <Tabs className='example-code-tabs'>
       <TabList>
-        <Tab disabled>JSON</Tab>
+        <Tab>JSON</Tab>
+        <Tab>XML</Tab>
       </TabList>
       <TabPanel>
-        <pre>
-          {`${reformattedCode}`}
-        </pre>
+        <pre dangerouslySetInnerHTML={{__html: `${reformattedJSON}`}} />
+      </TabPanel>
+      <TabPanel>
+        <pre dangerouslySetInnerHTML={{__html: `${reformattedXML}`}} />
       </TabPanel>
     </Tabs>
   );
 };
 
 CodeBlock.propTypes = {
-  code: PropTypes.string.isRequired
+  json: PropTypes.string.isRequired,
+  xml: PropTypes.string.isRequired
 };
 
 const DataTableWithHeader = ({fields, header}) => {
@@ -795,7 +825,7 @@ const DataTableWithHeader = ({fields, header}) => {
     return (
       <div className='ds-u-margin-top--2'>
         <h3 className='ds-h3'>{header}</h3>
-        <DataModelTable fields={fields}/>
+        <DataModelTable fields={fields} />
       </div>
     );
   } else {
@@ -803,14 +833,24 @@ const DataTableWithHeader = ({fields, header}) => {
   }
 };
 
+DataTableWithHeader.propTypes = {
+  fields: PropTypes.array,
+  header: PropTypes.string.isRequired
+};
+
 const MetadataMessagesTitle = () => <h2 className='ds-h2'>Metadata and Message Resource</h2>;
+
+const MetadataMessagePropType = PropTypes.shape({
+  metadata: PropTypes.arrayOf(PropTypes.object),
+  messages: PropTypes.arrayOf(PropTypes.object)
+});
 
 const MetadataMessages = ({base, ia, aci, quality}) => {
   if (Object.values(base).length > 0) {
     return (
       <div className='ds-u-margin-top--4'>
         <MetadataMessagesTitle />
-        <DataTableWithHeader fields={base.metadata} header='Metadata'/>
+        <DataTableWithHeader fields={base.metadata} header='Metadata' />
         <DataTableWithHeader fields={base.messages} header='Messages' />
       </div>
     );
@@ -818,11 +858,11 @@ const MetadataMessages = ({base, ia, aci, quality}) => {
     return (
       <div className='ds-u-margin-top--4'>
         <MetadataMessagesTitle />
-        <DataTableWithHeader fields={ia.metadata} header='Improvement Activities Metadata'/>
+        <DataTableWithHeader fields={ia.metadata} header='Improvement Activities Metadata' />
         <DataTableWithHeader fields={ia.messages} header='Improvement Activities Messages' />
-        <DataTableWithHeader fields={aci.metadata} header='Advancing Care Information Metadata'/>
+        <DataTableWithHeader fields={aci.metadata} header='Advancing Care Information Metadata' />
         <DataTableWithHeader fields={aci.messages} header='Advancing Care Information Messages' />
-        <DataTableWithHeader fields={quality.metadata} header='Quality Metadata'/>
+        <DataTableWithHeader fields={quality.metadata} header='Quality Metadata' />
         <DataTableWithHeader fields={quality.messages} header='Quality Messages' />
       </div>
     );
@@ -831,31 +871,47 @@ const MetadataMessages = ({base, ia, aci, quality}) => {
   }
 };
 
-const Resource = ({title, description, example, fields, metadata_messages}) => {
+MetadataMessages.propTypes = {
+  base: MetadataMessagePropType,
+  ia: MetadataMessagePropType,
+  aci: MetadataMessagePropType,
+  quality: MetadataMessagePropType
+};
+
+const Resource = ({id, title, description, example, fields, metadata_messages: metadataMessages}) => {
   return (
     <div className='ds-u-margin-bottom--4'>
-      <h1 className='ds-h1'>{title}</h1>
+      <h1 className='ds-h1' id={id}>{title}</h1>
       <p className='ds-text--lead'>{description}</p>
       <h2 className='ds-h2'>Resource Representation</h2>
-      <CodeBlock code={example} />
+      <CodeBlock {...example} />
       <DataModelTable fields={fields} />
-      <MetadataMessages {...metadata_messages} />
+      <MetadataMessages {...metadataMessages} />
     </div>
   );
 };
 
-const ResourceFieldPropType = PropTypes.shape({
+const ResourceFieldsPropType = PropTypes.shape({
   name: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   notes: PropTypes.string
 }).isRequired;
 
+const ResourceMetadataMessagesPropType = PropTypes.shape({
+  base: MetadataMessagePropType,
+  ia: MetadataMessagePropType,
+  aci: MetadataMessagePropType,
+  quality: MetadataMessagePropType
+});
+
 Resource.propTypes = {
+  id: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
-  example: PropTypes.string.isRequired,
-  fields: PropTypes.arrayOf(ResourceFieldPropType).isRequired
+  example: PropTypes.objectOf(PropTypes.string).isRequired,
+  fields: PropTypes.arrayOf(ResourceFieldsPropType).isRequired,
+  metadata_messages: ResourceMetadataMessagesPropType.isRequired
 };
 
 export default class ScoringEngine extends PureComponent {
@@ -866,6 +922,14 @@ export default class ScoringEngine extends PureComponent {
     return (
       <div id='scoring-engine'>
         <h1 className='ds-h1'>Scoring</h1>
+        <ul>
+          <li><a href={`#${SCORE_RESOURCE.id}`}>{SCORE_RESOURCE.title}</a></li>
+          <li><a href={`#${SCORE_PART_RESOURCE.id}`}>{SCORE_PART_RESOURCE.title}</a></li>
+          <li><a href={`#${CATEGORY_SCORE_RESOURCE.id}`}>{CATEGORY_SCORE_RESOURCE.title}</a></li>
+          <li><a href={`#${MEASUREMENT_SET_SCORE_PART_RESOURCE.id}`}>{MEASUREMENT_SET_SCORE_PART_RESOURCE.title}</a></li>
+          <li><a href={`#${MEASUREMENT_SCORE_PART_RESOURCE.id}`}>{MEASUREMENT_SCORE_PART_RESOURCE.title}</a></li>
+          <li><a href={`#${MEASUREMENT_SCORE_RESOURCE.id}`}>{MEASUREMENT_SCORE_RESOURCE.title}</a></li>
+        </ul>
         <h1 className='ds-h1'>Overview</h1>
         <p className='ds-text--lead'>
           The Scoring Engine resides within the QPP Submissions API application and calculates a performance score when it receives QPP submission data. By sending a submission through the Submissions API, the scoring engine evaluates the contained: provider profile information, measurement set performance data, and available benchmarking data, for each performance category.
@@ -878,10 +942,6 @@ export default class ScoringEngine extends PureComponent {
         </p>
         <p className='ds-text--lead'>
           Lastly, the Score Object is passed back to the QPP Submissions API, which builds the application response by inserting the Score Object into the response body and returns this response to the requester. This response body contains JSON describing in detail the record of the current aggregate estimate of the submission score.
-        </p>
-        <h1 className='ds-h1'>Score Object Navigation</h1>
-        <p className='ds-text--lead'>
-          Too be built...
         </p>
         <Resource {...SCORE_RESOURCE} />
         <Resource {...SCORE_PART_RESOURCE} />
