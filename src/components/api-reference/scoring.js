2daefbb6b5f6793eb6f21481e9f195178bcb469b
Roye Avraham <roye.avraham@gmail.com>
2017-11-29 16:08:35 -0500

    Adding for IA as well

--- src/components/api-reference/scoring.js
+++ src/components/api-reference/scoring.js
@@ -360,6 +360,12 @@ const MEASUREMENT_SET_SCORE_PART_RESOURCE = {
           value: 'boolean',
           description: 'Flag if the measurement set contributed to the final score',
           notes: ''
+        },
+        {
+          name: 'measurementSetId',
+          value: 'string',
+          description: 'Data store ID for the measurement’s measurement set',
+          notes: 'A V4 UUID'
         }
       ],
       messages: null
