5877282d111b8af5ce09480d6f5bc59a73b3d6b6
ivanang <ivana@navapbc.com>
2018-09-13 11:12:20 -0400

    Updating the 'get api key' section (#106)
    * Updating the 'get api key' section
    
    Added copy to the 'Get API key' section to explain how registries / qcdrs will receive their API key.
    
    * fixed doublequotes
    
    * Adding rel to targetblank link in 248
    
    * ran eslint
    
    * ran eslint again

--- src/components/developer-preview.js
+++ src/components/developer-preview.js
@@ -43,7 +43,10 @@ class DeveloperPreview extends React.Component {
         <a className='ds-c-button ds-c-button--primary' href='/qpp-submissions-docs/developer-preview#api-key'>Get an API Key</a>
 
         <h1 className='ds-h1' id='api-key'>Get an API Key</h1>
-        <p className='ds-text'>This content is not yet available. Please check back later.</p>
+        <p className='ds-text'>The Developer Preview is open to 2018 CMS-approved Qualified Registries ("registries") and Qualified Clinical Data Registries ("QCDRs"). Each 2018 registry and QCDR will receive a unique API key via email during the week of September 24, 2018. The email will come from <a href='mailto:partnership@cms.hhs.gov'>partnership@cms.hhs.gov</a>, and will be sent to the designated Technical Contact for the organization.</p>
+        <p className='ds-text'>The email will contain a secure URL, which can be used to access the API key. This API key will only work in the Developer Preview environment. It cannot be used for production submissions.</p>
+        <p className='ds-text'>Note that the URL expires a week from when it is sent, so we recommend that the recipient open it right away, and save the API key in a secure location.</p>
+        <p className='ds-text'>If you have any questions or issues getting your API key, please contact the Quality Payment Program Service Center at <a href='mailto:QPP@cms.hhs.gov'>QPP@cms.hhs.gov</a> or 1-866-288-8292 and ask to be put in touch with API Support.</p>
 
         <h1 className='ds-h1' id='authentication'>Authentication</h1>
         <p className='ds-text'>You must authenticate your account when using the Developer Preview (base url: <strong>https://preview.qpp.cms.gov/api/submissions/</strong>). Authenticate via bearer auth by adding your API token to the header of every request using the key value: <strong>Authorization: Bearer [YOUR API TOKEN]</strong>.</p>
@@ -242,7 +245,7 @@ class DeveloperPreview extends React.Component {
         <ul>
           <li>Send test API requests in the <a href='https://qpp-submissions-sandbox.navapbc.com/'>Interactive Documentation</a> using your Developer Preview API key.</li>
           <li>Contact the QPP Service Center.</li>
-          <li>Post product feedback about the Developer Preview in the <a href='https://groups.google.com/forum/#!forum/qpp-apis' target='_blank'>Google Group for QPP APIs</a>.</li>
+          <li>Post product feedback about the Developer Preview in the <a href='https://groups.google.com/forum/#!forum/qpp-apis' target='_blank' rel='noopener noreferrer'>Google Group for QPP APIs</a>.</li>
         </ul>
       </div>
     );
