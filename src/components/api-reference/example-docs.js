6baa771426b5591f53a82d5bb5830d7726a0ba34
Michael Conroy <sietekk@gmail.com>
2018-09-27 11:34:59 -0400

    Fixing Stevens mess :)

--- src/components/api-reference/example-docs.js
+++ src/components/api-reference/example-docs.js
@@ -5,22 +5,22 @@ import CopyToClipboard from 'react-copy-to-clipboard';
 import { pd } from 'pretty-data';
 import '../../styles/api-reference/example-docs.css';
 import '../../styles/common/example-code-tabs.css';
-import submissionJsonExample_ia from './common/submission-example-ia.json';
-import submissionJsonExample_aci from './common/submission-example-aci.json';
-import submissionJsonExample_quality from './common/submission-example-quality.json';
+import submissionJsonExampleIa from './common/submission-example-ia.json';
+import submissionJsonExampleAci from './common/submission-example-aci.json';
+import submissionJsonExampleQuality from './common/submission-example-quality.json';
 import submissionXmlExample from './common/submission-example-xml.js';
 
-const submissionJsonExampleString_ia = JSON.stringify(submissionJsonExample_ia, null, 4);
-const submissionJsonExampleString_aci = JSON.stringify(submissionJsonExample_aci, null, 4);
-const submissionJsonExampleString_quality = JSON.stringify(submissionJsonExample_quality, null, 4);
+const submissionJsonExampleStringIa = JSON.stringify(submissionJsonExampleIa, null, 4);
+const submissionJsonExampleStringAci = JSON.stringify(submissionJsonExampleAci, null, 4);
+const submissionJsonExampleStringQuality = JSON.stringify(submissionJsonExampleQuality, null, 4);
 const submissionXmlExampleString = pd.xml(submissionXmlExample);
 const cssClasses = 'example-code-tabs example-docs';
 
 function downloadJsonExample(e) {
   e.preventDefault();
-  fileDownload(submissionJsonExampleString_ia, 'submission-example-ia.json');
-  fileDownload(submissionJsonExampleString_aci, 'submission-example-aci.json');
-  fileDownload(submissionJsonExampleString_quality, 'submission-example-quality.json');
+  fileDownload(submissionJsonExampleStringIa, 'submission-example-ia.json');
+  fileDownload(submissionJsonExampleStringAci, 'submission-example-aci.json');
+  fileDownload(submissionJsonExampleStringQuality, 'submission-example-quality.json');
 }
 
 function downloadXmlExample(e) {
@@ -36,32 +36,34 @@ class ExampleDocs extends React.PureComponent {
       <div>
         <Tabs className={cssClasses}>
           <TabList>
-            <Tab>Sample JSON</Tab>
+            <Tab>Sample JSON IA</Tab>
+            <Tab>Sample JSON PI</Tab>
+            <Tab>Sample JSON Quality</Tab>
             <Tab>Sample XML</Tab>
           </TabList>
           <TabPanel className='ds-base--inverse'>
             <button className='ds-c-button ds-c-button--small ds-u-margin-right--1'
               onClick={downloadJsonExample}>Download IA</button>
-            <CopyToClipboard text={submissionJsonExampleString_ia}>
+            <CopyToClipboard text={submissionJsonExampleStringIa}>
               <button className='ds-c-button ds-c-button--small'>Copy to clipboard</button>
             </CopyToClipboard>
-            <pre>{`${submissionJsonExampleString_ia}`}</pre>
+            <pre>{`${submissionJsonExampleStringIa}`}</pre>
           </TabPanel>
           <TabPanel className='ds-base--inverse'>
             <button className='ds-c-button ds-c-button--small ds-u-margin-right--1'
               onClick={downloadJsonExample}>Download PI</button>
-            <CopyToClipboard text={submissionJsonExampleString_aci}>
+            <CopyToClipboard text={submissionJsonExampleStringAci}>
               <button className='ds-c-button ds-c-button--small'>Copy to clipboard</button>
             </CopyToClipboard>
-            <pre>{`${submissionJsonExampleString_aci}`}</pre>
+            <pre>{`${submissionJsonExampleStringAci}`}</pre>
           </TabPanel>
           <TabPanel className='ds-base--inverse'>
             <button className='ds-c-button ds-c-button--small ds-u-margin-right--1'
               onClick={downloadJsonExample}>Download Quality</button>
-            <CopyToClipboard text={submissionJsonExampleString_quality}>
+            <CopyToClipboard text={submissionJsonExampleStringQuality}>
               <button className='ds-c-button ds-c-button--small'>Copy to clipboard</button>
             </CopyToClipboard>
-            <pre>{`${submissionJsonExampleString_quality}`}</pre>
+            <pre>{`${submissionJsonExampleStringQuality}`}</pre>
           </TabPanel>
           <TabPanel>
             <button className='ds-c-button ds-c-button--small ds-u-margin-right--1'
