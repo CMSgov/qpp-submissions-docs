149f10ccae14ad7af4af5dc0dcae7c0788a54c62
StevenSzeliga <steven.szeliga@semanticbits.com>
2018-10-03 09:15:53 -0400

    Updating Scoring Docs. (#110)
    * Updating Scoring Docs.
    
    * Fixing Stevens mess :)

--- src/components/api-reference/example-docs.js
+++ src/components/api-reference/example-docs.js
@@ -5,16 +5,22 @@ import CopyToClipboard from 'react-copy-to-clipboard';
 import { pd } from 'pretty-data';
 import '../../styles/api-reference/example-docs.css';
 import '../../styles/common/example-code-tabs.css';
-import submissionJsonExample from './common/submission-example.json';
+import submissionJsonExampleIa from './common/submission-example-ia.json';
+import submissionJsonExampleAci from './common/submission-example-aci.json';
+import submissionJsonExampleQuality from './common/submission-example-quality.json';
 import submissionXmlExample from './common/submission-example-xml.js';
 
-const submissionJsonExampleString = JSON.stringify(submissionJsonExample, null, 4);
+const submissionJsonExampleStringIa = JSON.stringify(submissionJsonExampleIa, null, 4);
+const submissionJsonExampleStringAci = JSON.stringify(submissionJsonExampleAci, null, 4);
+const submissionJsonExampleStringQuality = JSON.stringify(submissionJsonExampleQuality, null, 4);
 const submissionXmlExampleString = pd.xml(submissionXmlExample);
 const cssClasses = 'example-code-tabs example-docs';
 
 function downloadJsonExample(e) {
   e.preventDefault();
-  fileDownload(submissionJsonExampleString, 'submission-example.json');
+  fileDownload(submissionJsonExampleStringIa, 'submission-example-ia.json');
+  fileDownload(submissionJsonExampleStringAci, 'submission-example-aci.json');
+  fileDownload(submissionJsonExampleStringQuality, 'submission-example-quality.json');
 }
 
 function downloadXmlExample(e) {
@@ -30,16 +36,34 @@ class ExampleDocs extends React.PureComponent {
       <div>
         <Tabs className={cssClasses}>
           <TabList>
-            <Tab>Sample JSON</Tab>
+            <Tab>Sample JSON IA</Tab>
+            <Tab>Sample JSON PI</Tab>
+            <Tab>Sample JSON Quality</Tab>
             <Tab>Sample XML</Tab>
           </TabList>
           <TabPanel className='ds-base--inverse'>
             <button className='ds-c-button ds-c-button--small ds-u-margin-right--1'
-              onClick={downloadJsonExample}>Download</button>
-            <CopyToClipboard text={submissionJsonExampleString}>
+              onClick={downloadJsonExample}>Download IA</button>
+            <CopyToClipboard text={submissionJsonExampleStringIa}>
               <button className='ds-c-button ds-c-button--small'>Copy to clipboard</button>
             </CopyToClipboard>
-            <pre>{`${submissionJsonExampleString}`}</pre>
+            <pre>{`${submissionJsonExampleStringIa}`}</pre>
+          </TabPanel>
+          <TabPanel className='ds-base--inverse'>
+            <button className='ds-c-button ds-c-button--small ds-u-margin-right--1'
+              onClick={downloadJsonExample}>Download PI</button>
+            <CopyToClipboard text={submissionJsonExampleStringAci}>
+              <button className='ds-c-button ds-c-button--small'>Copy to clipboard</button>
+            </CopyToClipboard>
+            <pre>{`${submissionJsonExampleStringAci}`}</pre>
+          </TabPanel>
+          <TabPanel className='ds-base--inverse'>
+            <button className='ds-c-button ds-c-button--small ds-u-margin-right--1'
+              onClick={downloadJsonExample}>Download Quality</button>
+            <CopyToClipboard text={submissionJsonExampleStringQuality}>
+              <button className='ds-c-button ds-c-button--small'>Copy to clipboard</button>
+            </CopyToClipboard>
+            <pre>{`${submissionJsonExampleStringQuality}`}</pre>
           </TabPanel>
           <TabPanel>
             <button className='ds-c-button ds-c-button--small ds-u-margin-right--1'
