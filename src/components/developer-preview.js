2b389f0b374ec5189964d610dbd538e2b7b13afd
Kalvin <82277+kalvinwang@users.noreply.github.com>
2019-04-02 19:09:03 -0700

    update introduction and dev preview pages

--- src/components/developer-preview.js
+++ src/components/developer-preview.js
@@ -40,14 +40,11 @@ class DeveloperPreview extends React.Component {
         <h2 className='ds-h2'>Authentication</h2>
         <p className='ds-text'>Both the Developer Preview and the Production environments require a JSON Web Token ("API key") for authentication purposes.</p>
 
-        <a className='ds-c-button ds-c-button--primary' href='/qpp-submissions-docs/developer-preview#api-key'>Get an API Key</a>
-
         <h1 className='ds-h1' id='api-key'>Get an API Key</h1>
-        <p className='ds-text'>The Developer Preview is open to 2018 CMS-approved Qualified Registries ("registries") and Qualified Clinical Data Registries ("QCDRs"). Each 2018 registry and QCDR will receive a unique API key via email during the week of September 24, 2018. The email will come from <a href='mailto:partnership@cms.hhs.gov'>partnership@cms.hhs.gov</a>, and will be sent to the designated Technical Contact for the organization.</p>
-        <p className='ds-text'>The email will contain a secure URL, which can be used to access the API key. This API key will only work in the Developer Preview environment. It cannot be used for production submissions.</p>
-        <p className='ds-text'>Note that the URL expires a week from when it is sent, so we recommend that the recipient open it right away, and save the API key in a secure location.</p>
-        <p className='ds-text'>If you have any questions or issues getting your API key, please contact the Quality Payment Program Service Center at <a href='mailto:QPP@cms.hhs.gov'>QPP@cms.hhs.gov</a> or 1-866-288-8292 and ask to be put in touch with API Support.</p>
-
+        <a className='ds-c-button ds-c-button--primary' href='/qpp-submissions-docs/developer-preview#api-key'>Get an API Key</a>
+        <p className='ds-text'>The Developer Preview is open year round and currently supports PY 2018 testing. As such, the Developer Preview is open to 2018 CMS-approved Qualified Registries ("registries") and Qualified Clinical Data Registries ("QCDRs").</p>
+        <p className='ds-text'>Developer Preview tokens for 2019 registries and QCDRs will become available when the testing environment switches from supporting PY 2018 to PY 2019 later this year.</p>
+        <p className='ds-text'>Anyone may continue to test the current iteration of the Developer Preview environment through our <a href='https://preview.qpp.cms.gov/api/submissions/public/docs/'>Interactive Documentation</a>. A default API token is embedded on this website, which you may also use for testing. To retrieve this token, test any endpoint on the website and then copy it from the API's cURL response.</p>
         <h1 className='ds-h1' id='authentication'>Authentication</h1>
         <p className='ds-text'>You must authenticate your account when using the Developer Preview (base url: <strong>https://preview.qpp.cms.gov/api/submissions/</strong>). Authenticate via bearer auth by adding your API token to the header of every request using the key value: <strong>Authorization: Bearer [YOUR API TOKEN]</strong>.</p>
         <p className='ds-text'>API keys carry many privileges, and must not be shared in publicly accessible areas such as GitHub and in client-side code.  Even within organizations, access must be limited to staff embedding it in software.</p>
--- src/components/introduction.js
+++ src/components/introduction.js
@@ -4,6 +4,28 @@ class Introduction extends React.Component {
   render() {
     return (
       <div>
+        <h2 className='ds-h2'>Quality Payment Program Submissions API Will Close for Official Reporting April 2, 2019 at 8 PM ET</h2>
+        <p className='ds-text'>Performance data for the 2018 Quality Payment Program (QPP) must be submitted prior to April 2, 2019 at 8 PM ET. After this date, 2018 Qualified Registries and Qualified Clinical Data Registries who have an API token will have limited access to specific endpoints of the Submissions API.</p>
+
+        <h2 className='ds-h2'>API Access After the 2018 QPP Submission Window Closes</h2>
+        <p className='ds-text'>After April 2, 2019, 2018 Qualified Registries and Qualified Clinical Data Registries who have an API token will not be able to access <strong>write</strong> and <strong>update</strong> endpoints in the production environment. In other words, the following endpoints, which were available during the submission window, will no longer be accessible:</p>
+
+        <p className='ds-text'>MeasurementSets
+          <ul>
+          <li>POST https://qpp.cms.gov/api/submissions/submissions/measurement-sets</li>
+          <li>PUT, PATCH, POST, DELETE https://qpp.cms.gov/api/submissions/submissions/measurement-sets/{'{'}id{'}'}</li>
+          </ul>
+        </p>
+        <p className='ds-text'>Measurements</p>
+        <ul>
+        <li>POST https://qpp.cms.gov/api/submissions/submissions/measurements</li>
+        <li>PUT, PATCH, DELETE https://qpp.cms.gov/api/submissions/submissions/measurements/{'{'}id{'}'}</li>
+        </ul>
+        <p className='ds-text'>Attempting to access any of these endpoints after April 2, 2019 at 8 PM ET will return a 401 error.</p>
+
+        <p className='ds-text'>POST https://qpp.cms.gov/api/submissions/submissions/score-preview will still be accessible, as this data is not persisted. Please remember that this endpoint will not return a score for special scoring scenarios and is set up for 2018 PY scoring logic.</p>
+
+        <p className='ds-text'>GET https://qpp.cms.gov/api/submissions/submissions/{'{'}id{'}'}/score will also remain accessible. Please note that this endpoint returns the score for a submission object, and does <strong>not</strong> represent the final score for a TIN/NPI.</p>
         <h2 className='ds-h2'>Easily submit and score QPP data in real-time via API</h2>
         <p className='ds-text'>The Submissions API enables submissions and real-time performance scoring of Quality Payment Program (QPP) data.</p>
         <p className='ds-text'>The Submissions API is a RESTful API. It has predictable, resource-oriented URLs, and uses HTTP response codes to indicate API errors. It uses standard HTTP features, like HTTPS authentication and HTTP verbs, which are understood by off-the-shelf HTTP clients.</p>
@@ -12,8 +34,7 @@ class Introduction extends React.Component {
 
         <h2 className='ds-h2'>Explore and integrate with the Submissions API</h2>
         <p className='ds-text'>To make the API as explorable as possible, we have <a href='https://preview.qpp.cms.gov/api/submissions/public/docs/'>Interactive Documentation</a>, which not only documents the endpoints available but also allows you to make requests to the API and see what response you would get. The Interactive Documentation is connected to the Developer Preview, which is an integration environment for registries and QCDRs.</p>
-        <p className='ds-text'>The <a href='https://cmsgov.github.io/qpp-submissions-docs/developer-preview'>Developer Preview environment</a> will be available in Fall 2018, and is open to 2018 CMS-approved Qualified Registries (“registries”) and Qualified Clinical Data Registries (“QCDRs”). Participants in the Developer Preview can build integrations with their software to test submission and scoring of Quality Payment Program performance data via API.</p>
-        <p className='ds-text'>Requests made in the Interactive Documentation and Developer Preview are not officially submitted to CMS.</p>
+        <p className='ds-text'>Requests made in the Interactive Documentation and Developer Preview are not officially submitted to CMS. You can learn more about the Developer Preview <a href='https://cmsgov.github.io/qpp-submissions-docs/developer-preview'>here</a></p>
 
         <h2 className='ds-h2'>Stay Up Date</h2>
         <p className='ds-text'>The <a href='https://groups.google.com/forum/#!forum/qpp-apis' target='_blank'>QPP APIs Google Group</a> is where developers and the CMS QPP Product Managers discuss your questions and feedback. New announcements and discussions are being added everyday.</p>
