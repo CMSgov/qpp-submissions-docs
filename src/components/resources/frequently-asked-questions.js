1e65d8e190f7e70bc26e1d25a7e7eac043b16ac8
Pete <peter.schnabel@semanticbits.com>
2020-08-10 10:30:36 -0400

    updated terms of service (#170)

--- src/components/resources/frequently-asked-questions.js
+++ src/components/resources/frequently-asked-questions.js
@@ -5,18 +5,18 @@ const tableData = {
   'If I am an EHR, how can I submit MIPS Quality data for my clients?': `
     <ul>
       <li>
-        Submissions UI: EHRs can submit via the Submissions UI by logging into the QPP site <a href="https://www.qpp.cms.gov">qpp.cms.gov</a>. They will have to be connected as a Staff user for each of the Individuals and Groups they need to submit quality data for. Submissions for each individual or group could be uploaded as an excel file for efficiency.
+        Submissions UI: EHRs can submit via the Submissions UI by logging into the QPP site <a href='https://www.qpp.cms.gov' target='_blank' rel='noopener noreferrer'>qpp.cms.gov</a>. They will have to be connected as a Staff user for each of the Individuals and Groups they need to submit quality data for. Submissions for each individual or group could be uploaded as an excel file for efficiency.
       </li>
       <li>
-        Burden Reduction: EHRs may apply for Burden Reduction which allows them to obtain a registry token to use to submit via the Submissions API. See the process outline in this <a href="https://groups.google.com/forum/#!msg/qpp-apis/EKqpKejepwY/uPrgllFiDQAJ">Google Group post</a>
+        Burden Reduction: EHRs may apply for Burden Reduction which allows them to obtain a registry token to use to submit via the Submissions API. See the process outline in this <a href='https://groups.google.com/forum/#!msg/qpp-apis/EKqpKejepwY/uPrgllFiDQAJ' target='_blank' rel='noopener noreferrer'>Google Group post</a>
       </li>
       <li>
-        OAUTH: You can use OAUTH to submit quality data to the Submissions API. See <a href="/getting-started-with-oauth2">Getting Started with OAUTH</a>.
+        OAUTH: You can use OAUTH to submit quality data to the Submissions API. See <a href='/getting-started-with-oauth2'>Getting Started with OAUTH</a>.
       </li>
     </ul>
   `,
   'If I am an APM entity, how can I submit MIPS Quality data?': `
-    You can use OAUTH to submit quality data to the Submissions API. See <a href="/getting-started-with-oauth2">Getting Started with OAUTH</a>.
+    You can use OAUTH to submit quality data to the Submissions API. See <a href='/getting-started-with-oauth2'>Getting Started with OAUTH</a>.
   `,
   'What is the performance threshold for the Submissions API?': '25 rps avg threshold, 40 rps burst threshold.'
 };
@@ -28,12 +28,12 @@ class FrequentlyAskedQuestions extends React.Component {
 
         <h3 className='ds-h3'>General</h3>
         <p>
-          See <a href='https://cmsgov.github.io/qpp-developer-preview-docs/frequently-asked-questions'>Developer Preview Frequently Asked Questions</a>
+          See <a href='https://cmsgov.github.io/qpp-developer-preview-docs/frequently-asked-questions' target='_blank' rel='noopener noreferrer'>Developer Preview Frequently Asked Questions</a>
         </p>
 
         <h3 className='ds-h3'>Developer Preview</h3>
         <p>
-          See <a href='https://cmsgov.github.io/qpp-developer-preview-docs/frequently-asked-questions'>Developer Preview Frequently Asked Questions</a>
+          See <a href='https://cmsgov.github.io/qpp-developer-preview-docs/frequently-asked-questions' target='_blank' rel='noopener noreferrer'>Developer Preview Frequently Asked Questions</a>
         </p>
 
         <h3 className='ds-h3'>Submissions API</h3>
