2047158939de8fe28664884cc5ea31377486578e
PetePlays <peteplays@hotmail.com>
2020-08-10 13:52:10 -0400

    updated text

--- src/components/references/schemas/submissions.js
+++ src/components/references/schemas/submissions.js
@@ -9,10 +9,11 @@ const FIELDS = [
   {name: 'id', value: 'string', description: 'The id of the submission.'},
   {name: 'createdAt', value: 'datetime', description: 'The creation time of the submission in <a href="https://www.ietf.org/rfc/rfc3339.txt">RFC 3339</a> format.'},
   {name: 'updatedAt', value: 'datetime', description: 'The modification time of the submission in <a href="https://www.ietf.org/rfc/rfc3339.txt">RFC 3339</a> format.'},
-  {name: 'entityType', value: 'string', description: 'The type of provider associated with the submission. Acceptable values are <b>"individual"</b> and <b>"group"</b>.', notes: 'writable'},
-  {name: 'taxpayerIdentificationNumber', value: 'string', description: 'The 9-digit identifier of the provider associated with the submission.', notes: 'writable'},
+  { name: 'entityType', value: 'string', description: 'Acceptable values are <b>"apm"</b>, <b>"group"</b>, <b>"individual"</b>, <b>"virtualGroup"</b>', notes: 'writable, required' },
+  {name: 'entityId', value: 'string', description: 'The unique identifier for the virtual group or APM associated with the submission.  If a CPC+ APM, the entityID is the CPC+ PracticeID', notes: 'writable, required if entityType is "apm" or "virtualGroup" '},
+  {name: 'taxpayerIdentificationNumber', value: 'string', description: 'The 9-digit identifier of the provider associated with the submission.', notes: 'writable if entityType is "individual" or "group"'},
   {name: 'nationalProviderIdentifier', value: 'string', description: 'The 10-digit identifier of the provider associated with the submission.', notes: 'writable only if entityType is "individual"'},
-  {name: 'performanceYear', value: 'integer', description: 'The year in which performance data for the submission was collected.', notes: 'writable'},
+  {name: 'performanceYear', value: 'integer', description: 'The year in which performance data for the submission was collected.', notes: 'writable, required'},
   {name: 'measurementSets', value: 'array(measurementSet)', description: 'Measurement sets associated with the submission.', notes: 'writable, optional'}
 ];
 
