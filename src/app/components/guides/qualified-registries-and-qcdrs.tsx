12a3968568caccb2618495951f5bb19ed800381e
ckawell <clkawell@outlook.com>
2021-01-05 14:04:35 -0600

    qppa-4535 Cleanup QCDR tab

--- src/app/components/guides/qualified-registries-and-qcdrs.tsx
+++ src/app/components/guides/qualified-registries-and-qcdrs.tsx
@@ -10,9 +10,14 @@ const tableData = [
     description: `<p>Returns all submissions that you have submitted measurement sets for, and within those submissions, returns only the measurement sets that you have  submitted. If <code>qpp-taxpayer-identification-number</code> is specified in the header as a query parameter, and you haven't submitted any measurement sets for the specified TIN, this endpoint returns a submission object with an empty measurement sets array.</p><p>A Registry token is allowed to see only the measurementSets and measurements that were submitted by that Registry.  A Registry token cannot see quality data submitted through other submissionMethods.</p>`,
   },
   {
+    endPoint: '/submissions',
+    method: 'POST',
+    description: `Returns '401 Unauthorized'. <code>POST /submissions</code> are not allowed, as once a submission object is created for an entity (TIN, TIN+NPI, entityID), a <code>POST /submissions</code> request will result in a duplicate entry error.  <code>POST /measurement-sets</code> should be used.`,
+  },
+  {
     endPoint: '/submissions/:id',
     method: 'GET',
-    description: `Returns '401 Unauthorized'.  Registry tokens are NOT allowed to do a POST /submission, as there are other users, as well as external systems, that POST /measurement-sets data for an entity which would cause the submission object to already be created for a TIN, TIN+NPI, or an entity.  Once a submission object is created for an entity a POST /submissions request would result in a duplicate entry error.  A submission object contains all of the submission data for an entity from all submissionMethods for a given Performance Year.`,
+    description: `Returns submissions for a specific submission ID.`,
   },
   {
     endPoint: '/measurement-sets',
