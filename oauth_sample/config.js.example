a9672e04ae3598ac6b2086240ab3ca65a9cb70eb
Nicholas Gates <nicholas.gates@semanticbits.com>
2021-08-25 12:34:05 -0400

    Updates

--- oauth_sample/config.js.example
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    CLIENT_ID: '',
-    CLIENT_SECRET: '',
-    APP_BASE_URL: 'http://localhost',
-    APP_PORT: '3000',
-    QPP_BASE_URL: 'http://preview.qpp.cms.gov/api/auth'
-};
--- src/shared/index.ts
+++
@@ -1,7 +0,0 @@
-export { LinkToId } from './link-to-id';
-export { ExternalLink } from './external-link';
-export { LockBodyScroll } from './lock-body-scroll';
-export * from './code-tab';
-export * from './api-example';
-export * from './data-model-table';
-
