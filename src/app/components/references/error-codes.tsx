776bde726123ff1f52a144c21a3e872ab85376ce
John Manack <john.manack@semanticbits.com>
2022-08-25 09:34:16 -0400

    QPPA-6757 remove bullet point and imbed link in header for dev pre error codes

--- src/app/components/references/error-codes.tsx
+++ src/app/components/references/error-codes.tsx
@@ -6,12 +6,7 @@ const ErrorCodes = () => {
       <p className='qpp-docs-page-updated'>Last Updated: 08/31/2022</p> {/* IMPORTANT: update this Last-Updated value if you have made any changes to this page's content. */}
       <h2 className='ds-h2' style={{marginTop: 0}} id='references'>Error Codes</h2>
 
-      <h3 className='ds-h3'>The Submissions API Error Codes summary can be found in the Developer Preview Documentation</h3>
-      <ul>
-        <li>
-          <ExternalLink href={`https://cmsgov.github.io/qpp-developer-preview-docs/error-codes`} />
-        </li>
-      </ul>
+      <h3 className='ds-h4'>The Submissions API Error Codes summary can be found in the <ExternalLink href={`https://cmsgov.github.io/qpp-developer-preview-docs/error-codes`} text='Developer Preview Documentation' /></h3>
     </>
   );
 };
