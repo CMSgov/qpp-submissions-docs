8b63fb9042c2877a1d758ff73dbd72e2314a8689
steven <steven.szeliga@icf.com>
2022-10-14 10:52:16 -0400

    added faq section

--- src/app/components/resources-and-support/frequently-asked-questions.tsx
+++ src/app/components/resources-and-support/frequently-asked-questions.tsx
@@ -16,6 +16,34 @@ const tableData = {
   `,
   'What is the performance threshold for the Submissions API?': '25 rps avg threshold, 40 rps burst threshold.',
 };
+const tableData1 = {
+  'How to report Measure 238 / CMS156': `
+    <ul>
+      <li>
+        MIPS CQM: This should follow the CQM Specification and 2 strata are expected to be reported.
+      </li>
+      <li>
+        eCQM: This should follow the eCQM Specification and 3 strata are expected to be reported. If mapping to QPP JSON, Numerator Exclusions should be mapped to the performanceNotMet field.
+      </li>
+    </ul>
+  `,
+  'How to report Measure 409':'The initial rate in the specifications should be followed to determine if the second rate used for performance can be submitted. If the thresholds outlined in the first strata are not met, this measure should not be reported.',
+  'Quality Measure Sort Order Issue':'This issue was resolved and deployed 10/6/2022.',
+  'PI Public Health and Clinical Data Exchange Bonus Submission':`
+  <p>Currently working on a fix to allow for the submission of any combination of:</p>
+    <ul>
+      <li>
+        PI_PHCDRR_2
+      </li>
+      <li>
+        PI_PHCDRR_4
+      </li>
+      <li>
+        PI_PHCDRR_5
+      </li>
+    </ul>
+      `  
+};
 
 const FrequentlyAskedQuestions = () => {
   return (
@@ -37,6 +65,25 @@ const FrequentlyAskedQuestions = () => {
         </thead>
         <tbody>
           {Object.entries(tableData).map(([question, answer], i) =>
+            <tr key={i}>
+              <td className='qpp-table-text-align'>{question}</td>
+              {envConfig.htmlRegex.test(answer)
+                ? <td className='qpp-table-text-align' dangerouslySetInnerHTML={{ __html: answer }} />
+                : <td className='qpp-table-text-align' >{answer}</td>
+              }
+            </tr>,
+          )}
+        </tbody>
+      </table>
+      <h3 className='ds-h3'>PY22 Questions and Known Issues</h3>
+      <table className='ds-c-table ds-u-font-size--small faq-table'>
+        <thead>
+          <tr>
+            {['Question', 'Answer'].map((h, i) => <th key={i}>{h}</th>)}
+          </tr>
+        </thead>
+        <tbody>
+          {Object.entries(tableData1).map(([question, answer], i) =>
             <tr key={i}>
               <td className='qpp-table-text-align'>{question}</td>
               {envConfig.htmlRegex.test(answer)
