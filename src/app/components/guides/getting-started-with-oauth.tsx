307ce45550f4652bc676a51b85f1c08b4cce1cad
John Manack <john.manack@semanticbits.com>
2023-08-17 13:02:02 -0400

    fix: QPPA-7725 removes trailing whitespace

--- src/app/components/guides/getting-started-with-oauth.tsx
+++ src/app/components/guides/getting-started-with-oauth.tsx
@@ -9,7 +9,7 @@ const GettingStartedUsingQppOauth = () => {
       <p className='qpp-docs-page-updated'>Last Updated: 08/31/2023</p> {/* IMPORTANT: update this Last-Updated value if you have made any changes to this page's content. */}
       <h2 className='ds-h2' style={{marginTop: 0}}>Getting Started Using QPP OAuth</h2>
       <p className='ds-text'>
-        Using OAuth with the Submissions API allows QPP participants to use their own QPP credentials to login through your application to submit their data to and view performance feedback from QPP. Before you can request production access, you will need to use the Developer Preview Environment to build, test and demo your OAuth application. 
+        Using OAuth with the Submissions API allows QPP participants to use their own QPP credentials to login through your application to submit their data to and view performance feedback from QPP. Before you can request production access, you will need to use the Developer Preview Environment to build, test and demo your OAuth application.
         </p>
 
       <h2 className='ds-h2' id='oauth'>Using OAuth in the Developer Preview</h2>
