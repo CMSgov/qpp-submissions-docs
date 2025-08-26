df03d75a8129672725fa17f5636ee1d5f25fcb29
John Manack <john.manack@semanticbits.com>
2025-08-26 14:58:18 -0400

    feat: QPPA-10471 rename Submission swagger docs to interactice docs

--- src/app/components/references/error-codes.tsx
+++ src/app/components/references/error-codes.tsx
@@ -8,7 +8,7 @@ const ErrorCodes: React.FC<DocPageProps> = ({dataTestId}: DocPageProps) => {
       <p className='qpp-docs-page-updated'>Last Updated: 08/28/2025</p> {/* IMPORTANT: update this Last-Updated value if you have made any changes to this page's content. */}
       <h2 className='ds-text-heading--2xl' style={{marginTop: 0}} id='references'>Error Codes</h2>
 
-      <h3 className='ds-text-heading--lg'>To review a list of response codes provided by the API, please visit the <ExternalLink href={`${envConfig.qppCmsPreviewUrl}/api/submissions/public/docs`} text='Submission swagger docs' />.</h3>
+      <h3 className='ds-text-heading--lg'>To review a list of response codes provided by the API, please visit the <ExternalLink href={`${envConfig.qppCmsPreviewUrl}/api/submissions/public/docs`} text='Interactive Docs' />.</h3>
     </div>
   );
 };
